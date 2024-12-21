42b848ab260dcd717e4a11d9e41a056968b4d211
Ankit Kumar <ankit933480@gmail.com>
2024-12-21 16:00:05 +0530

    Add files via upload

---
+++ assi-6/index.js
@@ -0,0 +1,6 @@
+
+const array = [1, 2, 3, 4, 5];
+const even = array.filter(num => num % 2 === 0);
+const odd = array.filter(num => num % 2 !== 0);
+console.log("Even Numbers:", even);
+console.log("Odd Numbers:", odd);
